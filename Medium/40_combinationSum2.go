25711347553d4d9acdc7b27318f8c68895bd126a
L1z1ng3r-sswe <krasavachic@gmail.com>
2024-06-10 21:22:22 +0600

    216_combinationSum3 done

--- Medium/40_combinationSum2.go
+++ Medium/40_combinationSum2.go
@@ -1,32 +1,31 @@
 func combinationSum2(candidates []int, target int) [][]int {
-	sort.Ints(candidates)
 	var res [][]int
+	sort.Ints(candidates)
 
-	var dfs func(index int, currNums []int, remaining int)
-	dfs = func(index int, currNums []int, remaining int) {
+	var dfs func(start int, currNums []int, remaining int)
+	dfs = func(start int, currNums []int, remaining int) {
 		if remaining == 0 {
 			res = append(res, append([]int(nil), currNums...))
 			return
 		}
+
 		if remaining < 0 {
 			return
 		}
 
-		// [1,1,2,5,6,7,10]
-		prev := -1
-		for i := index; i < len(candidates); i++ {
+		// [1,1,1,2,2], target = 4
+		var prev = -1
+		for i := start; i < len(candidates); i++ {
 			if prev == candidates[i] {
 				continue
 			}
 
 			currNums = append(currNums, candidates[i])
 			dfs(i+1, currNums, remaining-candidates[i])
-			//pop
 			currNums = currNums[:len(currNums)-1]
 			prev = candidates[i]
 		}
 	}
-
 	dfs(0, []int{}, target)
 
 	return res
