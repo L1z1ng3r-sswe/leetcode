5e150ea4e4254cf9f83ee91e027fe86ed3b69648
L1z1ng3r-sswe <krasavachic@gmail.com>
2024-06-19 22:29:33 +0600

    53_maxSubArray done O(N)

--- Medium/198_rob.go
+++ Medium/198_rob.go
@@ -1,19 +1,21 @@
 func rob(nums []int) int {
-	step1 := 0
-	step2 := 0
+	prevprev := 0
+	prev := 0
 
+	//[2,2000,9,3,1, 1000]
 	for _, num := range nums {
-		temp := max(step1+num, step2)
-		step1 = step2
-		step2 = temp
+		temp := max(prevprev+num, prev)
+		prevprev = prev
+		prev = temp
 	}
 
-	return step2
+	return prev
 }
 
 func max(n1, n2 int) int {
 	if n1 > n2 {
 		return n1
 	}
+
 	return n2
 }
--- Medium/53_maxSubArray.go
+++ Medium/53_maxSubArray.go
@@ -1,16 +1,13 @@
 func maxSubArray(nums []int) int {
-	result := nums[0]
-	current := 0
+	var res = nums[0]
+	var currentMax = nums[0]
 
 	for _, num := range nums {
-		if current < 0 {
-			current = 0
-		}
-		current += num
-		result = max(result, current)
+		currentMax = max(currentMax+num, num)
+		res = max(res, currentMax)
 	}
 
-	return result
+	return res
 }
 
 func max(n1, n2 int) int {
