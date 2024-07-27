6e3408ba9519aa4aca1897a16de596b5feb79f22
L1z1ng3r-sswe <krasavachic@gmail.com>
2024-07-27 20:04:45 +0500

    feat: 200_numIslands done; space: O(N*M), space: O(1)

---
+++ Medium/200_numIslands.go
@@ -0,0 +1,37 @@
+func numIslands(grid [][]byte) int {
+	if len(grid) == 0 {
+		return 0
+	}
+
+	rows := len(grid)
+	cols := len(grid[0])
+	numIslands := 0
+
+	var dfs func(int, int)
+	dfs = func(r int, c int) {
+		if r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] == '0' {
+			return
+		}
+
+		grid[r][c] = '0'
+
+		dfs(r+1, c)
+		dfs(r-1, c)
+		dfs(r, c+1)
+		dfs(r, c-1)
+	}
+
+	for r := 0; r < rows; r++ {
+		for c := 0; c < cols; c++ {
+			if grid[r][c] == '1' {
+				numIslands++
+				dfs(r, c)
+			}
+		}
+	}
+
+	return numIslands
+}
+
+// time: O(N*M)
+// space: O(1)
