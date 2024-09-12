335426212da47850af90500f063a5ed08d3c6807
L1z1ng3r-sswe <krasavachic@gmail.com>
2024-09-12 12:34:43 +0500

    235_lowestCommonAncestor done; time: O(logN), space: stack

--- Medium/235_lowestCommonAncestor.go
+++ Medium/235_lowestCommonAncestor.go
@@ -1,46 +1,15 @@
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-	pPath := targetPath(root, p)
-	qPath := targetPath(root, q)
-
-	var res *TreeNode
-
-	for i, j := len(pPath)-1, len(qPath)-1; i >= 0 && j >= 0; i, j = i-1, j-1 {
-		if pPath[i] != qPath[j] {
-			break
-		}
-
-		res = pPath[i]
+	if root.Val > p.Val && root.Val > q.Val {
+		return lowestCommonAncestor(root.Left, p, q)
 	}
 
-	return res
+	if root.Val < p.Val && root.Val < q.Val {
+		return lowestCommonAncestor(root.Right, p, q)
+	}
+
+	return root
 }
 
-func targetPath(root *TreeNode, target *TreeNode) []*TreeNode {
-	if root == nil {
-		return nil
-	}
-
-	if root == target {
-		return []*TreeNode{root}
-	}
-
-	if root.Left != nil {
-		path := targetPath(root.Left, target)
-		if path != nil {
-			return append(path, root)
-		}
-	}
-
-	if root.Right != nil {
-		path := targetPath(root.Right, target)
-		if path != nil {
-			return append(path, root)
-		}
-	}
-	return nil
-}
-
-// n - total number of nodes
-// h - height of the tree
-// time: O(n)
-// space: O(h)
+// N - total number of nodes
+// time: O(logN)
+// space: stack
