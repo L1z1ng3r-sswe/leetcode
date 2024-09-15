e70167702466ac85e8197789516e7ba6308e9426
L1z1ng3r-sswe <krasavachic@gmail.com>
2024-09-15 15:24:14 +0500

    feat: 720_longestWord done; time: O(nlogn), space: O(n)

---
+++ Medium/720_longestWord.go
@@ -0,0 +1,27 @@
+func longestWord(words []string) string {
+	var res string
+	sort.Strings(words)
+
+	hashMap := make(map[string]bool)
+	hashMap[""] = true
+
+	for _, word := range words {
+		if !hashMap[word[:len(word)-1]] {
+			continue
+		}
+
+		hashMap[word] = true
+		if len(word) > len(res) {
+			res = word
+		}
+	}
+
+	return res
+}
+
+// time: O(nlogn)
+// space: O(n)
+
+git add .
+git commit -m "feat: 720_longestWord done; time: O(nlogn), space: O(n)"
+git push origin main
