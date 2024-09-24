3c76f6bf43bc93ca896390a1b262511c152ac461
L1z1ng3r-sswe <krasavachic@gmail.com>
2024-09-24 13:51:23 +0500

    feat: 828_uniqueLetterString done; time: O(n), space: O(n)

---
+++ Hard/828_uniqueLetterString.go
@@ -0,0 +1,34 @@
+func uniqueLetterString(s string) int {
+	idx := make([byte][]int) // to track for each charracter in a string its prev1 and prev2 appearence
+
+	for i := 0; i < len(s); i++ {
+		char := s[i]
+		idx[char] = []int{-1, -1}
+	}
+
+	var res int
+
+	for i := 0; i < len(s); i++ {
+		char := s[i]
+
+		prev1 := idx[char][0]
+		prev2 := idx[char][1]
+
+		res += (prev1 - prev2) * (i - prev1)
+
+		idx[char] = []int{i, prev1} // new prev1 as i and new prev2 as the old prev1
+	}
+
+	// compute for the last positions
+	for _, arr := range idx {
+		prev1 := arr[0]
+		prev2 := arr[1]
+
+		res += (prev1 - prev2) * (n - prev1)
+	}
+
+	return res
+}
+
+// time: O(n)
+// space: O(n)
