d3fb7a0591db1c6a6b39261b3aad82bbbfbf7c43
L1z1ng3r-sswe <krasavachic@gmail.com>
2024-10-05 19:07:03 +0500

    feat: 124_maxPathSum done; time: O(h), space: O(1)

---
+++ Hard/124_maxPathSum.go
@@ -0,0 +1,35 @@
+func maxPathSum(root *TreeNode) int {
+	var maxPath = math.MinInt32
+
+	var dfs func(root *TreeNode) int
+	dfs = func(root *TreeNode) int {
+		if root == nil {
+			return 0
+		}
+
+		leftRes := max(0, dfs(root.Left))
+		rightRes := max(0, dfs(root.Right))
+
+		total := root.Val + leftRes + rightRes
+
+		maxPath = max(maxPath, total)
+
+		return root.Val + max(leftRes, rightRes)
+	}
+
+	dfs(root)
+
+	return maxPath
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
+// n - total number of nodes
+// time: O(n)
+// space: O(1)
